b35b2bc514966e766a54b0c69dec7174f1c95d9b
ASTARION <151060299+ASTARIONCrypto@users.noreply.github.com>
2023-11-29 16:48:11 +0100

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -12,13 +12,31 @@ try{
     const provider = new Provider({ rpc: { nodeUrl: General.nodeUrl } });
 
     const account = new Account(provider, AXcontractAddress, privateKeyAX, "1");
- 
+
+    if(General.contract == "NFT")
+    {
     const calldata = CallData.compile({ name: General.name, symbol: General.symbol, ownerof: AXcontractAddress });
  
     const { transaction_hash: mintTxHash }  =  await account.deployContract({ classHash: General.ClassHash, constructorCalldata: calldata });
     
 
     console.log("TR Hash: " + "https://starkscan.co/tx/" + mintTxHash) 
+        
+    }
+    
+    else if(General.contract == "Messenger")
+    {
+    const calldata = CallData.compile({ });
+ 
+    const { transaction_hash: mintTxHash }  =  await account.deployContract({ classHash: General.ClassHash, constructorCalldata: calldata });
+    
+
+    console.log("TR Hash: " + "https://starkscan.co/tx/" + mintTxHash) 
+        
+    }
+ 
+ 
+
     
 
     } catch(e) {
