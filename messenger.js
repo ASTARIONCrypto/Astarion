b7db2492fa89859ba64eb9a6e4dfc54094712887
ASTARION <151060299+ASTARIONCrypto@users.noreply.github.com>
2023-11-29 16:50:37 +0100

    Update and rename messenge.js to messenger.js

---
+++ messenger.js
@@ -0,0 +1,37 @@
+import {General} from './config.js';
+import {abi} from './data/abi.js';
+import fs from 'fs';
+import { Account, ec, json, stark, Provider, hash, constants, CallData, Contract } from "starknet";
+
+
+const privateKeyAX = fs.readFileSync('data/wallet.txt', 'utf8');
+const AXcontractAddress = fs.readFileSync('data/address.txt', 'utf8');
+
+try{
+
+    const provider = new Provider({ rpc: { nodeUrl: General.nodeUrl } });
+
+    const account = new Account(provider, AXcontractAddress, privateKeyAX, "1");
+
+
+    const { transaction_hash: mintTxHash }  = await account.execute(
+        [
+        // Calling the first contract
+        {
+        contractAddress: General.contractAddress,
+        entrypoint: "withdraw",
+        // approve 1 wei for bridge
+        calldata: CallData.compile({
+            amount: General.amounttoWithdraw,
+          })
+        }
+      ]
+    )
+    
+
+    console.log("TR Hash: " + "https://starkscan.co/tx/" + mintTxHash) 
+    
+
+    } catch(e) {
+    
+    console.log(e)}
